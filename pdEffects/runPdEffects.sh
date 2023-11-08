0821ee09d39dda33714915ffb7db17ec7b6af3da
John <john.harrison@alum.mit.edu>
2023-11-08 16:32:26 -0600

    pdEffects/runPdEffects.sh

--- pdEffects/runPdEffects.sh
+++ pdEffects/runPdEffects.sh
@@ -2,5 +2,9 @@
 
 
 cd /home/glog/Desktop/WW_Control/pdEffects
-echo glog | sudo alsa force-reload
-pd -audioindev 5,7,9,12 -inchannels 1,1,1,1 -audiooutdev 3 -outchannels 1 effectTry1.pd
+echo glog | sudo -S alsa force-reload
+if [ "X$(aplay -l | grep "card 0" | grep USB)" == "X" ]; then
+    pd -audioindev 5,7,9,12 -inchannels 1,1,1,1 -audiooutdev 3 -outchannels 1 effectTry1.pd
+else
+    pd -audioindev 1,3,5,7 -inchannels 1,1,1,1 -audiooutdev 11 -outchannels 1 effectTry1.pd
+fi
