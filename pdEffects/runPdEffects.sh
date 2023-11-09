98fd8d4c1e9464b2b63481c683bdad26e2842209
John <john.harrison@alum.mit.edu>
2023-11-09 10:41:32 -0600

    effects triage1

--- pdEffects/runPdEffects.sh
+++ pdEffects/runPdEffects.sh
@@ -4,7 +4,7 @@
 cd /home/glog/Desktop/WW_Control/pdEffects
 echo glog | sudo -S alsa force-reload
 if [ "X$(aplay -l | grep "card 0" | grep USB)" == "X" ]; then
-    pd -audioindev 5,7,9,12 -inchannels 1,1,1,1 -audiooutdev 3 -outchannels 1 effectTry1.pd
+    pd -audioindev 5,7,9,11 -inchannels 1,1,1,1 -audiooutdev 13 -outchannels 1 effectTry1.pd
 else
-    pd -audioindev 1,3,5,7 -inchannels 1,1,1,1 -audiooutdev 11 -outchannels 1 effectTry1.pd
+    pd -audioindev 1,3,5,7 -inchannels 1,1,1,1 -audiooutdev 9 -outchannels 1 effectTry1.pd
 fi
